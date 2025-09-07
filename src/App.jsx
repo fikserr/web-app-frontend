a10e30335974e246a785bf855e06081927cfda79
fikserr <azizxon7777777@gmail.com>
2025-09-07 12:40:15 +0500

    add user

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Bar from "./components/bar";
 import Header from "./components/header";
+import axios from "axios";
 
 const App = () => {
-  const location = useLocation();
-  const hideBarRoutes = ["/detail", "/categories"];
-  const shouldHideBar = hideBarRoutes.some((path) =>
-    location.pathname.startsWith(path)
-  );
+//   const location = useLocation();
+//   const hideBarRoutes = ["/detail", "/categories"];
+//   const shouldHideBar = hideBarRoutes.some((path) =>
+//     location.pathname.startsWith(path)
+//   );
+
+
+//   return (
+//     <div className="min-h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+//       <Header />
+//       <main className="pb-16">
+//         <Outlet />
+
+//       </main>
+//       {!shouldHideBar && <Bar />}
+//     </div>
+//   );
+// };
+
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const tg = window.Telegram.WebApp;
-    tg.ready(); // WebApp ishga tayyor
-    tg.expand(); // butun ekran qilib ochadi
-    alert(tg.initDataUnsafe?.user.id)
-    console.log("Telegram WebApp foydalanuvchi:", tg.initDataUnsafe?.user);
-  }, []);
-  return (
-    <div className="min-h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-      <Header />
-      <main className="pb-16">
-        <Outlet />
+    if (window.Telegram?.WebApp?.initDataUnsafe?.user) {
+      const tgUser = window.Telegram.WebApp.initDataUnsafe.user;
+      setUser(tgUser);
 
-      </main>
-      {!shouldHideBar && <Bar />}
+      // backendga yuborish
+      axios.post("https://telegram-web-app-backend.laravel.cloud/api/telegram/save-user", {
+        telegram_id: tgUser.id,
+        username: tgUser.username,
+        first_name: tgUser.first_name,
+        last_name: tgUser.last_name,
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      {user ? (
+        <p>Salom, {user.first_name} (ID: {user.id})</p>
+      ) : (
+        <p>Telegram user topilmadi</p>
+      )}
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    allowedHosts: ["05c6fbf9a885.ngrok-free.app"], // ngrok domeni shu yerga yoziladi
+  },
 })
