da2bbb963ae89f8ae303db81350add356192da31
fikserr <azizxon7777777@gmail.com>
2025-09-13 11:48:00 +0500

    fix some bugs 3

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from "react";
-import Logo from "../../assets/Logo.png";
 import { Moon, Sun } from "lucide-react"; // ikonalar
+import Logo from '../../assets/prosys.png'
 
 const Header = () => {
-    const [theme, setTheme] = useState(
-        localStorage.getItem("theme") || "light"
-    );
+    // const [theme, setTheme] = useState(
+    //     localStorage.getItem("theme") || "light"
+    // );
 
-    useEffect(() => {
-        if (theme === "dark") {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-        localStorage.setItem("theme", theme);
-    }, [theme]);
+    // useEffect(() => {
+    //     if (theme === "dark") {
+    //         document.documentElement.classList.add("dark");
+    //     } else {
+    //         document.documentElement.classList.remove("dark");
+    //     }
+    //     localStorage.setItem("theme", theme);
+    // }, [theme]);
+    const tg = window.Telegram.WebApp;
+
+    // light yoki dark qaytadi
+    console.log("Theme:", tg.colorScheme);
+
+    if (tg.colorScheme === "dark") {
+        document.documentElement.classList.add("dark"); // masalan Tailwind uchun
+    } else {
+        document.documentElement.classList.remove("dark");
+    }
+
 
     return (
-        <div className="px-5 xl:px-10 py-2 dark:bg-gray-800 text-gray-900 bg-white dark:text-gray-100 shadow-md fixed w-full top-0 z-50">
+        <div className="px-5 xl:px-10 h-16 py-1 dark:bg-gray-800 text-gray-900 bg-white dark:text-gray-100 shadow-md fixed w-full top-0 z-50 ">
             <div className="my-3 flex items-center justify-between">
                 {/* Logo */}
-                <img src={Logo} alt="Logo" className="h-10" />
+                <img src={Logo} alt="Logo" className="h-10 dark:shadow-md dark:shadow-white " />
 
                 {/* Toggle button */}
                 <button
