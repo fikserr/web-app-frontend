37683b18f6f775d5a77d2202533e50088c40652c
Abdumalik-Abdullayev1 <168909983+Abdumalik-Abdullayev1@users.noreply.github.com>
2025-09-23 20:49:04 +0500

    test

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react"; // ikonalar
-import Logo from '../../assets/prosys.png'
+import Logo from '../../assets/logo.png' // logo
 
 const Header = () => {
     const tg = window.Telegram.WebApp;
