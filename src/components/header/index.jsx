e7b3fa7952587f758b623d930387a06b2e03b428
ADMIN <azizxon7777777@gmail.com>
2025-09-24 11:19:23 +0500

    fix logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react"; 
-import Logo from '../../assets/logo.png' 
+import Logo from '../../assets/Logo.png' 
 
 const Header = () => {
     const tg = window.Telegram.WebApp;
