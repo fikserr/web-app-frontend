91ae63edb5593ea0e641490a9b2965dc63318205
mukhammadiyev <muhammadsolihmuhammadiyev2009@gmail.com>
2025-10-03 18:05:58 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import Bar from "./components/bar";
-import Header from "./components/header";
+import { Outlet, useLocation } from 'react-router-dom'
+import Bar from './components/bar'
+import Header from './components/header'
 
 const App = () => {
-  const location = useLocation();
-  const hideBarRoutes = ["/detail", "/categories"];
-  const shouldHideBar = hideBarRoutes.some((path) =>
-    location.pathname.startsWith(path)
-  );
-  const tg = window.Telegram?.WebApp;
-  tg?.ready();
+	const location = useLocation()
+	const hideBarRoutes = ['/detail', '/categories']
+	const shouldHideBar = hideBarRoutes.some(path =>
+		location.pathname.startsWith(path)
+	)
+	// const tg = window.Telegram?.WebApp;
+	// tg?.ready();
 
-  return (
-    <div className="min-h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-      <Header />
-      <main className="">
-        <Outlet />
-      </main>
-      {!shouldHideBar && <Bar />}
-    </div>
-  );
-};
+	return (
+		<div className='min-h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300'>
+			<Header />
+			<main className=''>
+				<Outlet />
+			</main>
+			{!shouldHideBar && <Bar />}
+		</div>
+	)
+}
 
-export default App;
+export default App
